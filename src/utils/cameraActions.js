df5416e274ccc120a808106522a8f8d59adcb732
Fabian Yate <fabianyate31@gmail.com>
2023-09-01 09:38:25 -0500

    Add new actions

---
+++ src/utils/cameraActions.js
@@ -0,0 +1,52 @@
+export const startCameraStream = async (videoRef) => {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: true,
+    });
+
+    videoRef.current.srcObject = stream;
+    videoRef.current.play();
+  } catch (error) {
+    throw new Error("Could not start camera: " + error.message);
+  }
+};
+
+export const stopCameraStream = (videoRef) => {
+  const stream = videoRef.current.srcObject;
+
+  if (stream) {
+    stream.getTracks().forEach((track) => track.stop());
+  }
+
+  videoRef.current.srcObject = null;
+};
+
+export const startAutomaticCapture = (interval, isCameraOn, handleCapture) => {
+  if (interval === null) {
+    const newInterval = setInterval(() => {
+      if (isCameraOn) {
+        handleCapture();
+      }
+    }, 5000);
+    return newInterval;
+  }
+};
+
+export const stopAutomaticCapture = (interval) => {
+  clearInterval(interval);
+};
+
+export const handleCapture = (captureImage, setCapturedImages) => {
+  const newImage = captureImage();
+
+  if (newImage) {
+    setCapturedImages((prevImages) => [...prevImages, newImage]);
+  }
+};
+
+export const handleDeleteImage = (index, capturedImages, setCapturedImages) => {
+  const newCapturedImages = [...capturedImages];
+  newCapturedImages.splice(index, 1);
+  setCapturedImages(newCapturedImages);
+};
