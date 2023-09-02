23cd373043abd8201c8bba77a44279ae5b4d1429
Fabian Yate <fabianyate31@gmail.com>
2023-09-02 03:18:28 -0500

    Update styles

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 import styles from "./Button.module.css";
 
-function Button({ onClick, label, title, children }) {
+function Button({ onClick, label, title, isActive, children }) {
   return (
-    <button className={styles.toggleButton} onClick={onClick} title={title}>
+    <button
+      className={`${styles.toggleButton} ${
+        isActive === false
+          ? styles.toggleButtonInactive
+          : styles.toggleButtonActive
+      }`}
+      onClick={onClick}
+      title={title}
+    >
       {children}
       {label}
     </button>
