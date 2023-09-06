39df4af165dfade31cf4a37dcf5587ddd0257695
Fabian Yate <fabianyate31@gmail.com>
2023-09-06 02:12:07 -0500

    Add function to download images

--- src/components/CaptureOptions.jsx
+++ src/components/CaptureOptions.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import styles from "./CaptureOptions.module.css";
 import AutomaticCaptureToggle from "./AutomaticCaptureToggle";
 import CaptureIntervalSelector from "./CaptureIntervalSelector";
+import { downloadAllImages } from "../utils/cameraActions";
+import Button from "./Button";
+import download from "../images/download.svg";
 
 function CaptureOptions({
   isAutoCaptureOn,
   toggleAutoCapture,
   customInterval,
   onChangeInterval,
+  capturedImages,
   id,
 }) {
   return (
@@ -24,6 +28,16 @@ function CaptureOptions({
           id={id}
           isAutoCaptureOn
         />
+      )}
+      {capturedImages.length > 0 && (
+        <Button
+          title="Descargar todas las imágenes"
+          label="Descargar todas las imágenes"
+          onClick={() => downloadAllImages(capturedImages)}
+          backgroundColor="#007BFF"
+        >
+          <img src={download} alt="Download all snapshots" />
+        </Button>
       )}
     </div>
   );
