9a8e75e82defe21c3a8ca00109b3b1236da2346b
Fabian Yate <fabianyate31@gmail.com>
2023-09-07 22:17:25 -0500

    Add function to delete an image

--- src/components/CaptureList.jsx
+++ src/components/CaptureList.jsx
@@ -3,6 +3,9 @@ import styles from "./CaptureList.module.css";
 import "react-medium-image-zoom/dist/styles.css";
 import deleteImage from "../images/delete.svg";
 import { Modal } from "./Modal";
+import Button from "./Button";
+import { downloadImage } from "../utils/filesActions";
+import download from "../images/download.svg";
 
 function CaptureList({ capturedImages, onDeleteImage }) {
   if (!Array.isArray(capturedImages) || capturedImages.length === 0) {
@@ -45,16 +48,20 @@ function CaptureList({ capturedImages, onDeleteImage }) {
             {groupedImages[monthYear].map((image, index) => (
               <li className={styles.captureList} key={index}>
                 <Modal alt={`Captured ${index}`} src={image.src} />
-                <button
-                  title="Delete image"
-                  onClick={() => onDeleteImage(index)}
-                >
-                  <img
-                    className={styles.deleteIcon}
-                    src={deleteImage}
-                    alt="Delete snapshot"
-                  />
-                </button>
+                <div className={styles.actions}>
+                  <Button
+                    title="Eliminar imagem"
+                    onClick={() => onDeleteImage(index)}
+                  >
+                    <img src={deleteImage} alt="Delete capture" />
+                  </Button>
+                  <Button
+                    title="Descargar imagem"
+                    onClick={() => downloadImage(image)}
+                  >
+                    <img src={download} alt="Download all snapshots" />
+                  </Button>
+                </div>
               </li>
             ))}
           </ul>
--- src/utils/filesActions.js
+++ src/utils/filesActions.js
@@ -47,3 +47,21 @@ export const handleDeleteImage = (index, capturedImages, setCapturedImages) => {
     setCapturedImages(newCapturedImages);
   }
 };
+
+const base64ToBlob = (base64, mimeType = 'image/png') => {
+  const byteString = atob(base64.split(',')[1]);
+  const ab = new ArrayBuffer(byteString.length);
+  const ia = new Uint8Array(ab);
+
+  for (let i = 0; i < byteString.length; i++) {
+      ia[i] = byteString.charCodeAt(i);
+  }
+
+  return new Blob([ab], { type: mimeType });
+};
+
+export const downloadImage = (image) => {
+  const fileName = `${formattedDate(image.timestamp)}.png`;
+  const blob = base64ToBlob(image.src);
+  saveAs(blob, fileName);
+};
