40c844fd48be1db31d51ee20a212794ac6b37949
Fabian Yate <fabianyate31@gmail.com>
2023-10-15 01:15:26 -0500

    Refactor

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -11,22 +11,26 @@ function Tabs({ tabs }) {
 
   return (
     <>
-      <div className={styles.tab_buttons}>
-        {tabs.map((tab) => (
-          <Button
-            key={tab.id}
-            label={tab.label}
-            isActive={activeTab === tab.id}
-            onClick={() => handleTabClick(tab.id)}
-            title={tab.label}
-            backgroundColor={tab.background}
-            activeBackgroundColor={tab.activeBackgroundColor}
-            inactiveBackgroundColor={tab.inactiveBackgroundColor}
-          >
-            <img src={tab.src} alt={tab.alt} />
-          </Button>
-        ))}
-      </div>
+      <nav>
+        <ul className={styles.tab_buttons}>
+          {tabs.map((tab) => (
+            <li className={styles.tab_item}>
+              <Button
+                key={tab.id}
+                label={tab.label}
+                isActive={activeTab === tab.id}
+                onClick={() => handleTabClick(tab.id)}
+                title={tab.label}
+                backgroundColor={tab.background}
+                activeBackgroundColor={tab.activeBackgroundColor}
+                inactiveBackgroundColor={tab.inactiveBackgroundColor}
+              >
+                <img src={tab.src} alt={tab.alt} />
+              </Button>
+            </li>
+          ))}
+        </ul>
+      </nav>
       <div className={styles.tab_content}>
         {tabs.find((tab) => tab.id === activeTab).content}
       </div>
